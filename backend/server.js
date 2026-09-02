0baad22b1c423f3bec74935e2b5e6b5f4726682b
Afrah Khan <itsafrahkhan10@gmail.com>
2026-09-02 22:01:03 +0530

    Add dashboard statistics API

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const dustbinRoutes = require("./routes/dustbinRoutes");
 const readingRoutes = require("./routes/readingRoutes");
 const collectionRoutes = require("./routes/collectionRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 const PORT = 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/dustbins", dustbinRoutes);
 app.use("/api/readings", readingRoutes);
 app.use("/api/collections", collectionRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.json({
